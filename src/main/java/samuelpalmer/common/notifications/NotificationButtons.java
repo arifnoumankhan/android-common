3fe347bfbbb255585b815c7f606f5cefc2f05fe9
Sam Palmer <sampalmer@users.noreply.github.com>
2015-12-03 22:16:37 +1100

    Fixed notification button animation bugs.

--- src/main/java/samuelpalmer/common/notifications/NotificationButtons.java
+++ src/main/java/samuelpalmer/common/notifications/NotificationButtons.java
@@ -1,11 +1,9 @@
 package samuelpalmer.common.notifications;
 
-import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.widget.RemoteViews;
 
 import samuelpalmer.common.R;
@@ -15,34 +13,19 @@ public class NotificationButtons {
 	private final RemoteViews content;
 	private final Context context;
 
-	public NotificationButtons(Context context) {
+	public NotificationButtons(Context context, int layoutResource) {
 		this.context = context;
-		content = new RemoteViews(context.getPackageName(), R.layout.notification_buttons);
+		content = new RemoteViews(context.getPackageName(), layoutResource);
 		content.removeAllViews(R.id.notification_buttons_main_content);
-		content.removeAllViews(R.id.notification_buttons_container);
 	}
 
-	/**
-	 * One of the LAYOUT_DIRECTION_ constants
-	 * @see android.view.View
-	 */
-	@TargetApi(VERSION_CODES.JELLY_BEAN_MR1)
-	public NotificationButtons setLayoutDirection(int layoutDirection) {
-		content.setInt(R.id.notification_buttons_container, "setLayoutDirection", layoutDirection);
-		return this;
-	}
-
-	public NotificationButtons configureButton(int icon, PendingIntent onClickIntent, CharSequence contentDescription) {
-		RemoteViews button = new RemoteViews(context.getPackageName(), R.layout.notification_button);
-
-		button.setImageViewResource(R.id.notification_button, icon);
-		button.setInt(R.id.notification_button, "setColorFilter", actionButtonColour(icon));
-		button.setOnClickPendingIntent(R.id.notification_button, onClickIntent);
+	public NotificationButtons configureButton(int id, int icon, PendingIntent onClickIntent, CharSequence contentDescription) {
+		content.setImageViewResource(id, icon);
+		content.setInt(id, "setColorFilter", actionButtonColour(icon));
+		content.setOnClickPendingIntent(id, onClickIntent);
 
 		if (VERSION.SDK_INT >= 15)
-			button.setContentDescription(R.id.notification_button, contentDescription);
-
-		content.addView(R.id.notification_buttons_container, button);
+			content.setContentDescription(id, contentDescription);
 
 		return this;
 	}
